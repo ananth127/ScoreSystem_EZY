26fa4c29dca92af20da8daa71822e7bb0d42a742
ananth127 <ananths12704@gmail.com>
2025-11-26 18:31:45 +0530

    first commit

--- scoresystem/storage/gameStorage.ts
+++ scoresystem/storage/gameStorage.ts
@@ -1,14 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { STORAGE_KEYS } from "../constants/storageKeys";
+import { Val_10, Val_0 } from "../constants/baseValue";
 
 export const gameStorage = {
   async getHighScore(): Promise<number> {
     try {
       const value = await AsyncStorage.getItem(STORAGE_KEYS.HIGH_SCORE);
-      return value ? parseInt(value, 10) : 0;
+      return value ? parseInt(value, Val_10) : Val_0;
     } catch (error) {
       console.error("getHighScore error:", error);
-      return 0;
+      return Val_0;
     }
   },
 
